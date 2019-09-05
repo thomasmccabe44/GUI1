403c9f115d2dfcd4a961d5e063d3c7a218a7b054
Thomas McCabe <thomas.mccabe.44@gmail.com>
2019-09-04 20:50:25 -0400

    Upload of all completed GUI 1 HW

---
+++ hw16/main.js
@@ -0,0 +1,104 @@
+// Get the Mark V Modal
+var modal = document.getElementById("mvModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("mv");
+var modalImg = document.getElementById("mv01");
+var captionText = document.getElementById("mvcaption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Get the Mark XL Modal
+var modal = document.getElementById("mxlModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("mxl");
+var modalImg = document.getElementById("mxl01");
+var captionText = document.getElementById("mxlcaption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Get the Mark XLII Modal
+var modal = document.getElementById("mxliiModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("mxlii");
+var modalImg = document.getElementById("mxlii01");
+var captionText = document.getElementById("mxliicaption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[2];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Get the Mark V Modal
+var modal = document.getElementById("mviiModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("mvii");
+var modalImg = document.getElementById("mvii01");
+var captionText = document.getElementById("mviicaption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[3];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Get the Mark XLVI Modal
+var modal = document.getElementById("mxlviModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("mxlvi");
+var modalImg = document.getElementById("mxlvi01");
+var captionText = document.getElementById("mxlvicaption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[4];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}

2a330a0284c05e705b8cc260f71ebc73059782c7
Thomas McCabe <thomas.mccabe.44@gmail.com>
2019-09-13 08:14:15 -0400

    updates to hw22 falling stars

---
+++ hw22/scripts.js
@@ -0,0 +1,16 @@
+let colors = ['yellow','pink','purple','lightblue','hotpink','teal', 'lavender','white','cyan','aqua','chartreuse','fuschia','tomato','aquamarine','lightgreen','gainsboro','aliceblue','orchid','deeppink','blueviolet','salmon','chocolate','cornflowerblue','crimson','skyblue','ghostwhite','honeydew','indigo','lime','paleturquoise','plum','thistle','springgreen','peachpuff','gold','floralwhite','transparent','turquoise','orange'],
+    stars = Math.floor(Math.random() * 200);
+
+const generateStars = () => {
+    for(let index=0; index < stars; index++) {
+        let star = document.createElement('div');
+
+        star.style.color = colors[Math.floor(Math.random() * colors.length)];
+        star.className = `fas fa fa-${Math.floor(Math.random() * 6)}x fa-star`;
+
+        document.querySelector('.galaxy').append(star);
+    }
+};
+
+
+generateStars();
---
+++ hw25/README.md
@@ -0,0 +1,31 @@
+[200~
+25 - create a page that use  jQuery
+
+	*
+watch jQuery on Youtube to get a sense
+
+What is jQuery ?
+Why use jQuery ?
+	*
+take jQuery ET class on Lynda, pick the one with better rating.
+	*
+create index.html, css, js, jQuery
+	*
+create a table
+	*
+create 2 buttons : hide + show
+	*
+click on hide will hide the table
+	*
+click on show will show the table
+	*
+test your work locally
+	*
+push to GitHub, and invite me
+	*
+deploy to GitHub Page, send me the link
+	*
+done ✅
+
+:wq!
+

906dda75dd9247fd95f749994b053636b06b6432
Thomas McCabe <thomas.mccabe.44@gmail.com>
2019-09-13 15:03:00 -0400

    beautified files

--- hw22/scripts.js
+++ hw22/scripts.js
@@ -1,8 +1,8 @@
-let colors = ['yellow','pink','purple','lightblue','hotpink','teal', 'lavender','white','cyan','aqua','chartreuse','fuschia','tomato','aquamarine','lightgreen','gainsboro','aliceblue','orchid','deeppink','blueviolet','salmon','chocolate','cornflowerblue','crimson','skyblue','ghostwhite','honeydew','indigo','lime','paleturquoise','plum','thistle','springgreen','peachpuff','gold','floralwhite','transparent','turquoise','orange'],
+let colors = ['yellow', 'pink', 'purple', 'lightblue', 'hotpink', 'teal', 'lavender', 'white', 'cyan', 'aqua', 'chartreuse', 'fuschia', 'tomato', 'aquamarine', 'lightgreen', 'gainsboro', 'aliceblue', 'orchid', 'deeppink', 'blueviolet', 'salmon', 'chocolate', 'cornflowerblue', 'crimson', 'skyblue', 'ghostwhite', 'honeydew', 'indigo', 'lime', 'paleturquoise', 'plum', 'thistle', 'springgreen', 'peachpuff', 'gold', 'floralwhite', 'transparent', 'turquoise', 'orange'],
     stars = Math.floor(Math.random() * 200);
 
 const generateStars = () => {
-    for(let index=0; index < stars; index++) {
+    for (let index = 0; index < stars; index++) {
         let star = document.createElement('div');
 
         star.style.color = colors[Math.floor(Math.random() * colors.length)];

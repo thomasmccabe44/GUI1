f5aca6343c74fe1ca981cbdce5268243bf610c29
Thomas McCabe <thomas.mccabe.44@gmail.com>
2019-10-07 10:06:53 -0400

    adjustments to scripts. js to produce end results with jQuery

--- hw31/scripts.js
+++ hw31/scripts.js
@@ -5,16 +5,16 @@ var carTypes = [
     " Hatchback"
 ];
 
-for (i=0; i<carTypes.length; i++) {
+for (i = 0; i < carTypes.length; i++) {
     $('#carTypes').append(carTypes[i]);
 };
 
 var truckTypes = [
     " Single Cab",
-    " Double Cab",
+    " Extended Cab",
     " Crew"
 ];
-for (i=0; i<truckTypes.length; i++) {
+for (i = 0; i < truckTypes.length; i++) {
     $('#truckTypes').append(truckTypes[i]);
 };
 
@@ -23,12 +23,40 @@ var suvTypes = [
     " Fullsized",
     " Compact"
 ];
-for (i=0; i<suvTypes.length; i++) {
+for (i = 0; i < suvTypes.length; i++) {
     $('#suvTypes').append(suvTypes[i]);
 };
 
 // Integers
+var int1 = [
+    "234 ",
+    "789 ",
+    "456"
+];
 
+for (i = 0; i < int1.length; i++) {
+    $('#int1').append(int1[i]);
+};
+
+var int2 = [
+    "2+1 ",
+    "3+1 ",
+    "4+1 "
+];
+
+for (i = 0; i < int2.length; i++) {
+    $('#int2').append(int2[i]);
+};
+
+var int3 = [
+    "1.1.1 ",
+    "1.1.2 ",
+    "1.1.3 "
+];
+
+for (i = 0; i < int3.length; i++) {
+    $('#int3').append(int3[i]);
+};
 
 
 
@@ -36,26 +64,28 @@ for (i=0; i<suvTypes.length; i++) {
 // Images
 
 var carImgs = [
-    ""
+    "https://www.topgear.com/sites/default/files/styles/16x9_1280w/public/images/news-article/2019/07/336b815c306ad46777b96aaf6e06fd72/2020-chevrolet-corvette-stingray-001.jpg?itok=7gNIGTN9 ",
+    "https://static.carthrottle.com/workspace/uploads/posts/2016/02/760f9a3ce49c35ae31f9beb66eaf8319.jpg ",
+    "https://www.theengineblock.com/wp-content/uploads/2018/12/2019-Hyundai-Veloster-N-00010-e1545168009313.jpg "
 ];
-for (i=0; i<carImgs.length; i++) {
+for (i = 0; i < carImgs.length; i++) {
     $('#carImgs').append(carImgs[i]);
 };
 
 var truckImgs = [
-    " ",
-    " ",
-    " "
+    "http://carphotos.cardomain.com/ride_images/4/465/1861/38660930001_original.jpg ",
+    "https://i.pinimg.com/originals/1a/60/da/1a60dab095be045bf657f8f3320711c5.jpg ",
+    "https://c9c16d2bac1d15766e14-54d46a283a18ac53a91fb3374695f975.ssl.cf1.rackcdn.com/1GC1KTEY4KF250016/bf00c36d6e83fcdb91dce6bbc05458e6.jpg "
 ];
-for (i=0; i<truckImgs.length; i++) {
+for (i = 0; i < truckImgs.length; i++) {
     $('#truckImgs').append(truckImgs[i]);
 };
 
 var suvImgs = [
-    " ",
-    " ",
-    " "
+    "https://www.chevrolet.com/content/dam/chevrolet/na/us/english/index/vehicles/2020/crossovers-suvs/blazer/mov/01-images/2020-blazer-masthead-01.jpg?imwidth=960 ",
+    "https://www.tfltruck.com/wp-content/uploads/2018/07/2019_dodge_durango_gtof84f1sei2u28m2bae1k1u5p4e-1024x683.jpghttps://www.tfltruck.com/wp-content/uploads/2018/07/2019_dodge_durango_gtof84f1sei2u28m2bae1k1u5p4e-1024x683.jpg ",
+    "https://suvs.com/images/best-2018-hyundai-kona-new-release.jpg "
 ];
-for (i=0; i<suvImgs.length; i++) {
+for (i = 0; i < suvImgs.length; i++) {
     $('#suvImgs').append(suvImgs[i]);
 };

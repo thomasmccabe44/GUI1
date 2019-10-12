942ee2d4496f033fdffc49222c0ec74d1dab6b93
Thomas McCabe <thomas.mccabe.44@gmail.com>
2019-10-12 17:59:08 -0400

    hw 32 walkthrough with bunglong table.html

---
+++ hw32/scripts.js
@@ -0,0 +1,19 @@
+// Test your work
+// console.log(skills);
+
+//assign variables from your data
+var name = skills.name;
+var type = skills.type;
+var value = skills.value;
+
+//inject skills to your HTML DOM
+
+for (i = 0; i < skills.length; i++) {
+    // console.log(skills[i]);
+
+    let name = skills[i].name;
+    let type = skills[i].type;
+    let value = skills[i].value;
+
+    $('#skillsTable ').append('<tr><td class="text-left">'+ name + ' </td><td>' + type + '</td><td>' + value + '</td></tr>');
+}

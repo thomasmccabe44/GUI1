61c08ec847ed7b2eeab12e75cfb03fde3479ebc3
Thomas McCabe <thomas.mccabe.44@gmail.com>
2019-10-12 18:35:08 -0400

    data prepped for graph

--- hw32/scripts.js
+++ hw32/scripts.js
@@ -3,21 +3,19 @@
 
 //assign variables from your data
 var name = skills.name;
-var type = skills.type;
 var value = skills.value;
 
 //inject skills to your HTML DOM
+// for graph.html
+var labels = [];
+var data = [];
 
 for (i = 0; i < skills.length; i++) {
-    console.log(skills[i]);
+    // console.log(skills[i]);
 
-    let name = skills[i].name;
-    let type = skills[i].type;
-    let value = skills[i].value;
+    labels.push(skills[i].name);
+    data.push(parseInt(skills[i].value));
 
-// for table.html
-    // $('#skillsTable ').append('<tr><td class="text-left">'+ name + ' </td><td>' + type + '</td><td>' + value + '</td></tr>');
-
-// for graph.html
-$('#canvas ').append('<tr><td class="text-left">'+ name + ' </td><td>' + type + '</td><td>' + value + '</td></tr>');
 }
+console.log("labels", labels);
+console.log("data", data);
